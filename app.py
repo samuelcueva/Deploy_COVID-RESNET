463c252fd49fc90d12c69b20683061c98fb514f3
Samuel Cueva Lozano <scueval07@gmail.com>
2021-07-09 15:09:09 -0500

    better performance

--- app.py
+++ app.py
@@ -3,41 +3,35 @@ import tensorflow as tf
 from model import preprocess_image
 import numpy as np
 
-IMAGE_SIZE=(256,256)
-PATHOLOGIES=['COVID-19','NORMAL','PNEUMONIA']
-path_img='images/image.jpg'
+IMAGE_SIZE = (256, 256)
+PATHOLOGIES = ['COVID-19', 'NORMAL', 'PNEUMONIA']
+path_img = 'images/image.jpg'
 # To load the serialized model
-model=tf.keras.models.load_model('covid_resnet_model')
+model = tf.keras.models.load_model('covid_resnet_model')
 
-
-app=Flask(__name__)
-
+app = Flask(__name__)
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+
+@app.route('/predict', methods=['POST'])
 def predict():
-    print(type(request.files['image']))
-    request.files['image'].save(path_img) #this save the image in a path but in production maybe it's not the better
-    output = model.predict(preprocess_image(path_img,IMAGE_SIZE))[0]
+    # print(type(request.files['image']))
+    request.files['image'].save(
+        path_img)  # this save the image in a path but in production maybe it's not the better
+    output = model.predict(preprocess_image(path_img, IMAGE_SIZE))[0]
 
     prediction = PATHOLOGIES[np.argmax(output)]
-    p=output[np.argmax(output)]*100
-    print(output)
-    string_test='this is a test'
+    p = output[np.argmax(output)] * 100
 
     return render_template('index.html',
-                           prediction='The x-ray result is: {} with a {:.2f} % probability'.format(prediction,p))
-    #return
+                           prediction='The x-ray result is: {} with a {:.2f} % probability'.format(
+                               prediction, p))
+    # return
 
-@app.route('/test' ,methods=['POST'])
-def test_design():
-    request.files['image'].save(path_img)
 
-    return
-
-if __name__=='__main__':
-    app.run(port=5000,debug=True)
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
--- model.py
+++ model.py
@@ -1,59 +1,46 @@
-import numpy as np
 import tensorflow as tf
-import os
-import pickle
 
-path_weights='weights_covidResnet/cp-0030.ckpt'
-path_images='resnet_images'
+path_weights = 'weights_covidResnet/cp-0030.ckpt'
+path_images = 'resnet_images'
 
-IMAGE_SIZE=(256,256)
-LR=0.01
-PATHOLOGIES=['COVID-19','NORMAL','PNEUMONIA']
 
-def preprocess_image(path,image_size):
-    raw_img=tf.keras.preprocessing.image.load_img(path)
-    img_array=tf.keras.preprocessing.image.img_to_array(raw_img)
-    img=tf.keras.preprocessing.image.smart_resize(img_array,image_size)
-    img=tf.expand_dims(img,0)
+def preprocess_image(path, image_size):
+    raw_img = tf.keras.preprocessing.image.load_img(path)
+    img_array = tf.keras.preprocessing.image.img_to_array(raw_img)
+    img = tf.keras.preprocessing.image.smart_resize(img_array, image_size)
+    img = tf.expand_dims(img, 0)
     return img
 
+
 def create_covidResnet(base_model):
-  """create top layers to customize ResNet50
-     to a network for Covid-19 detection"""
+    """create top layers to customize ResNet50
+       to a network for Covid-19 detection"""
 
-  input=tf.keras.Input(shape=(256,256,3))
-  preprocess_input=tf.keras.applications.resnet.preprocess_input(input)
-  model_base=base_model(preprocess_input)
-  global_average_layer=tf.keras.layers.GlobalAveragePooling2D()(model_base)
-  drop_out_1=tf.keras.layers.Dropout(0.4)(global_average_layer)
-  dense_layer=tf.keras.layers.Dense(4096,activation='relu')(drop_out_1)
-  drop_out_2=tf.keras.layers.Dropout(0.4)(dense_layer)
-  output_model=tf.keras.layers.Dense(3,activation='softmax')(drop_out_2)
+    input = tf.keras.Input(shape=(256, 256, 3))
+    preprocess_input = tf.keras.applications.resnet.preprocess_input(input)
+    model_base = base_model(preprocess_input)
+    global_average_layer = tf.keras.layers.GlobalAveragePooling2D()(model_base)
+    drop_out_1 = tf.keras.layers.Dropout(0.4)(global_average_layer)
+    dense_layer = tf.keras.layers.Dense(4096, activation='relu')(drop_out_1)
+    drop_out_2 = tf.keras.layers.Dropout(0.4)(dense_layer)
+    output_model = tf.keras.layers.Dense(3, activation='softmax')(drop_out_2)
 
-  #create the model
-  model=tf.keras.models.Model( inputs=input,outputs=output_model)
+    # create the model
+    model = tf.keras.models.Model(inputs=input, outputs=output_model)
 
-  return model
-
-# create the base model(Resnet 50)
-resnet50=tf.keras.applications.ResNet50( include_top=False,input_shape=(256,256,3) ,weights=None)
-
-# create COVID-RESNET
-covid_resnet=create_covidResnet(resnet50)
-
-# Load the trained weights into the model
-covid_resnet.load_weights('weights_covidResnet/cp-0030.ckpt')
-
-# Save the trained model
-covid_resnet.save('covid_resnet_model')
+    return model
 
 
-restored_model = tf.keras.models.load_model('covid_resnet_model')
+if __name__ == "__main__":
+    # create the base model(Resnet 50)
+    resnet50 = tf.keras.applications.ResNet50(include_top=False, input_shape=(256, 256, 3),
+                                              weights=None)
 
+    # create COVID-RESNET
+    covid_resnet = create_covidResnet(resnet50)
 
+    # Load the trained weights into the model
+    covid_resnet.load_weights(path_weights)
 
-output=restored_model.predict(preprocess_image(os.path.join(path_images,'covid_2.jpg'),IMAGE_SIZE))
-
-prediction=PATHOLOGIES[np.argmax(output)]
-
-print(prediction)
+    # Save the trained model
+    covid_resnet.save('covid_resnet_model')
