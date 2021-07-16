1d825531f369b743c16563babbfb8e0ab573145f
Samuel Cueva Lozano <scueval07@gmail.com>
2021-07-15 22:56:17 -0500

    correction of some errors

--- main.py
+++ main.py
@@ -2,12 +2,23 @@ from flask import Flask, render_template, request,jsonify
 import tensorflow as tf
 from model import preprocess_image
 import numpy as np
+import os
+from download_model import download
+
 
 IMAGE_SIZE = (256, 256)
 PATHOLOGIES = ['COVID-19', 'NORMAL', 'PNEUMONIA']
 path_img = 'images/image'
+
+# parameters for Google Cloud Storage API
+path_model = 'covid_resnet_model/'
+bucket_name = os.environ.get('BUCKET_NAME')
+blob = 'covid_resnet_model/'
+
+# to download the serialized model
+download(bucket_name,blob,path_model)
 # To load the serialized model
-model = tf.keras.models.load_model('covid_resnet_model')
+model = tf.keras.models.load_model(path_model)
 
 app = Flask(__name__)
 
